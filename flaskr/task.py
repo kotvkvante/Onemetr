c73ec0ea7d2896ca912451b2383a55e449bb5717
kotvkvante <kot-b-kbahte@yandex.ru>
2023-05-31 20:11:24 +0300

    solver for task4

--- flaskr/task.py
+++ flaskr/task.py
@@ -10,7 +10,6 @@ bp = Blueprint("task", __name__)
 
 @bp.route("/task4", methods=("GET", ))
 def task4():
-
     return render_template("tasks/task4.html")
 
 
@@ -31,11 +30,51 @@ def task4_solution():
 
     # ["/usr/bin/git", "commit", "-m", "Fixes a bug."]
 
-    path = "Onemetr/solver/a.out"
+    path = "Onemetr/solver/task4.bin"
     if current_app.debug:
-        path = "solver/a.out"
+        path = "solver/task4.bin"
 
     cmd = [path, R1, R2, R3, Rsum, EsR1, EsR2, EsR3, EsRsum, EiR1, EiR2, EiR3, EiRsum]
+    print(cmd)
+
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = p.wait()
+    a, b = p.communicate()
+    stdout, stderr = a.decode("utf-8"), b.decode("utf-8")
+    print("stdout {} >> {}".format(type(stdout), stdout))
+    print("stderr >> ", stderr)
+    if (stdout == ""):
+        return "Incorrect input"
+
+    res = stdout.split(" ")
+    print(res)
+
+    return "Rmax = {}\nRmin = {}".format(res[0], res[1])
+
+@bp.route("/task3", methods=("GET", ))
+def task3():
+    return render_template("tasks/task3.html")
+
+@bp.route("/task3_solution", methods=("POST", ))
+def task3_solution():
+    dmax = request.form.get("dmax")
+    dmin = request.form.get("dmax")
+    Smax = request.form.get("Smax")
+    Smin = request.form.get("Smin")
+    EPCmax = request.form.get("EPCmax")
+    EPCmin = request.form.get("EPCmin")
+    Dmax = request.form.get("Dmax")
+    Dmin = request.form.get("Dmin")
+
+    method = request.form.get("options")
+    # print(method)
+    # ["/usr/bin/git", "commit", "-m", "Fixes a bug."]
+
+    path = "Onemetr/solver/task3.bin"
+    if current_app.debug:
+        path = "solver/task3.bin"
+
+    cmd = [path, method, dmax, dmin, Smax, Smin, EPCmax, EPCmin, Dmax, Dmin]
     print(cmd)
 
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
