bbb02bef57d212bee98ead49d1df1a6d55ae3b55
kotvkvante <kot-b-kbahte@yandex.ru>
2023-05-31 20:28:34 +0300

    solver for task4

--- solver/task3.cpp
+++ solver/task3.cpp
@@ -43,8 +43,8 @@ int main(int argc, char *argv[])
 
     L = d / 2 + D / 2 + S - EPC;
 
-    cout << L;
-    
+    cout << L << " ";
+
     if (k == 1)
     {
         EsL = EsS + Eid / 2 + EiD / 2 - EsEPC;
@@ -90,51 +90,4 @@ int main(int argc, char *argv[])
         cout << EsL << " " << EiL;
         return 0;
     }
-
-
-}
-
-void Task4() {
-
-    const double R1 = 205, R2 = 218, R3 = 209, R_sum = 476,
-        Es_R1 = 1.9, Es_R2 = 1.4, Es_R3 = 1.2, Es_R_sum = 1.2,
-        Ei_R1 = -1.2, Ei_R2 = -1.1, Ei_R3 = -1.6, Ei_R_sum = -1.7;
-
-    double R23, R4;
-    double TR1, TR2, TR3, TR4, TR_sum;
-    double EmR1, EmR2, EmR3, EmR4, EmR_sum;
-    double delta2, delta3;
-    double EsR4, EiR4;
-    double R4_max, R4_min;
-
-    R23 = (R2 * R3) / (R2 + R3);
-    R4 = R_sum - R1 - R23;
-
-    TR1 = Es_R1 - Ei_R1;
-    TR2 = Es_R2 - Ei_R2;
-    TR3 = Es_R3 - Ei_R3;
-    TR_sum = Es_R_sum - Ei_R_sum;
-
-    EmR1 = (Es_R1 + Ei_R1) / 2;
-    EmR2 = (Es_R2 + Ei_R2) / 2;
-    EmR3 = (Es_R3 + Ei_R3) / 2;
-    EmR_sum = (Es_R_sum + Ei_R_sum) / 2;
-
-    delta2 = pow(R3,2) / pow(R2 + R3,2);
-    delta3 = pow(R2, 2) / pow(R2 + R3, 2);
-
-    TR4 = TR1 + delta2 * TR2 + delta3 * TR3 - TR_sum;
-    EmR4 = EmR_sum - EmR1 - delta2 * EmR2 - delta3 * EmR3;
-
-    EsR4 = EmR4 + TR4 / 2;
-    EiR4 = EmR4 - TR4 / 2;
-
-    R4_max = R4 + EsR4;
-    R4_min = R4 + EiR4;
-
-    cout << "R4_max = " << R4_max << endl;
-    cout << "R4_min = " << R4_min << endl;
-
-
-
 }
